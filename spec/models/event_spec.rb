8040130458e890b1c27296b0f6a7e1c9264afa4f
Korab Hoxha <korab-h@hotmail.com>
2017-07-08 12:50:36 +0200

    Added extra db_column validations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,17 +5,34 @@ describe Event, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:url) }
-    
+
     it { should validate_presence_of(:start_date) }
     it { should validate_presence_of(:end_date) }
     it { should validate_presence_of(:created) }
     it { should validate_presence_of(:updated) }
-    
+
     it { should validate_presence_of(:status) }
 
     it do
-    should define_enum_for(:status).
-      with(['canceled', 'live', 'started', 'ended', 'completed'])
+      should define_enum_for(:status).
+        with(['canceled', 'live', 'started', 'ended', 'completed'])
+    end
+
+    it do
+      should have_db_column(:shareable).
+        with_options(default: true)
+    end
+    it do
+      should have_db_column(:show_remaining).
+        with_options(default: true)
+    end
+    it do
+      should have_db_column(:listed).
+        with_options(default: true)
+    end
+    it do
+      should have_db_column(:invite_only).
+        with_options(default: false)
     end
   end
 
