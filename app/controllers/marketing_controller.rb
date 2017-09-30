efac5cb45fee5a394be712b30cc572a4e0bce5b1
Korab Hoxha <korab-h@hotmail.com>
2017-09-30 14:35:07 +0200

    LandingPage on MarketingController

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -3,11 +3,11 @@ class MarketingController < ApplicationController
     # sort_by
     # default_order
     @events = case params[:order_by] ? params[:order_by] : 'upcoming'
-    when 'upcoming'
-      landing_page.upcoming_events
-    when 'newest'
-      landing_page.recently_published_events
-    end
+              when 'upcoming'
+                landing_page.upcoming_events
+              when 'newest'
+                landing_page.recently_published_events
+              end
   end
 
   private
