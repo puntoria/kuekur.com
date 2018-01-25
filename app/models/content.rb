f733c69626420c0c1ef51ee2b238619e9bd8cf7e
Korab Hoxha <korab-h@hotmail.com>
2018-01-25 23:32:57 +0100

    Faq: Content

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,7 @@
 class Content < ApplicationRecord
   belongs_to :section, touch: true
 
-  validates :title, :description, :locale, presence: true
+  validates :title, :description, presence: true
   validates_associated :section
   validates :section_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 class ActionDispatch::Routing::Mapper
   def draw(routes_name)
     instance_eval(File.read(
-      Rails.root.join("config/routes/#{routes_name}.rb")
+                    Rails.root.join("config/routes/#{routes_name}.rb")
     ))
   end
 end
 
 Rails.application.routes.draw do
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   constraints Clearance::Constraints::SignedIn.new do
-    get "/" => 'marketing#index'
+    get '/' => 'marketing#index'
   end
 
   constraints Clearance::Constraints::SignedOut.new do
-    root to: "marketing#index"
+    root to: 'marketing#index'
   end
 
   draw :user
